99f19f740042d8bfb81523394909e8811efc9de3
RohanVDvivedi <rohandvivedi@gmail.com>
2023-07-03 04:05:47 -0700

    started with implementing initialize function for wale_init

--- src/wale_init.c
+++ src/wale_init.c
@@ -2,19 +2,26 @@
 
 #include<master_record_io.h>
 
+#include<stdlib.h>
+
+#define OS_PAGE_SIZE 4096
+
 int initialize_wale(wale* wale_p, uint64_t next_log_sequence_number, pthread_mutex_t* external_lock, block_io_ops block_io_functions, uint64_t append_only_block_count)
 {
 	wale_p->has_internal_lock = (external_lock == NULL);
 
 	if(wale_p->has_internal_lock)
-		pthread_mutex_init(&(wale_p->internal_lock));
+		pthread_mutex_init(&(wale_p->internal_lock), NULL);
 	else
 		wale_p->external_lock = external_lock;
 
 	wale_p->block_io_functions = block_io_functions;
 
 	if(next_log_sequence_number == INVALID_LOG_SEQUENCE_NUMBER)
-		read_master_record(&(wale_p->on_disk_master_record), &(wale_p->block_io_functions));
+	{
+		if(!read_master_record(&(wale_p->on_disk_master_record), &(wale_p->block_io_functions)))
+			return 0;
+	}
 	else
 	{
 		wale_p->on_disk_master_record.first_log_sequence_number = 0;
@@ -22,17 +29,21 @@ int initialize_wale(wale* wale_p, uint64_t next_log_sequence_number, pthread_mut
 		wale_p->on_disk_master_record.check_point_log_sequence_number = 0;
 		wale_p->on_disk_master_record.next_log_sequence_number = next_log_sequence_number;
 
-		write_and_flush_master_record(&(wale_p->on_disk_master_record), &(wale_p->block_io_functions));
+		if(!write_and_flush_master_record(&(wale_p->on_disk_master_record), &(wale_p->block_io_functions)))
+			return 0;
 	}
 
 	wale_p->in_memory_master_record = wale_p->on_disk_master_record;
 
-	wale_p->buffer = malloc(append_only_block_count * wale_p->block_io_functions.block_size);
+	wale_p->buffer = aligned_alloc(OS_PAGE_SIZE, (append_only_block_count * wale_p->block_io_functions.block_size));
 	wale_p->buffer_block_count = append_only_block_count;
+
+	if(wale_p->buffer == NULL)
+		return 0;
 
 	// read appropriate first block from memory
 	wale_p->append_offset = 0;
-	wale_p->buffer_start_block_id;
+	wale_p->buffer_start_block_id = 1;
 
 	wale_p->scrolling_up_append_only_buffer_in_progress = 0;
 	wale_p->flush_in_progress = 0;
