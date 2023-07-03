3a443ca588ef0430a9bdc347951c7baed25edba8
RohanVDvivedi <rohandvivedi@gmail.com>
2023-07-03 07:05:33 -0700

    started implementing main access functions for wale

--- src/wale.c
+++ src/wale.c
@@ -1,1 +1,19 @@
 #include<wale.h>
+
+uint64_t get_first_log_sequence_number(wale* wale_p);
+
+uint64_t get_last_flushed_log_sequence_number(wale* wale_p);
+
+uint64_t get_check_point_log_sequence_number(wale* wale_p);
+
+uint64_t get_next_log_sequence_number_of(wale* wale_p, uint64_t log_sequence_number);
+
+uint64_t get_prev_log_sequence_number_of(wale* wale_p, uint64_t log_sequence_number);
+
+void* get_log_record_at(wale* wale_p, uint64_t log_sequence_number, uint32_t* log_record_size);
+
+uint64_t append_log_record(wale* wale_p, const void* log_record, uint32_t log_record_size, int is_check_point);
+
+uint64_t flush_all_log_records(wale* wale_p);
+
+int truncate_log_records(wale* wale_p);
