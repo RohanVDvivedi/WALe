bc795693132807554523fd61d5c0f54679cdd099
RohanVDvivedi <rohandvivedi@gmail.com>
2023-07-03 09:12:36 -0700

    started with implementing append_log_record method

--- src/wale.c
+++ src/wale.c
@@ -20,7 +20,7 @@ uint64_t get_first_log_sequence_number(wale* wale_p)
 
 	pthread_mutex_lock(get_wale_lock(wale_p));
 
-	wale_p->random_readers_count++;
+	wale_p->random_readers_count--;
 
 	if(wale_p->has_internal_lock)
 		pthread_mutex_unlock(get_wale_lock(wale_p));
@@ -41,7 +41,7 @@ uint64_t get_last_flushed_log_sequence_number(wale* wale_p)
 
 	pthread_mutex_lock(get_wale_lock(wale_p));
 
-	wale_p->random_readers_count++;
+	wale_p->random_readers_count--;
 
 	if(wale_p->has_internal_lock)
 		pthread_mutex_unlock(get_wale_lock(wale_p));
@@ -62,7 +62,7 @@ uint64_t get_check_point_log_sequence_number(wale* wale_p)
 
 	pthread_mutex_lock(get_wale_lock(wale_p));
 
-	wale_p->random_readers_count++;
+	wale_p->random_readers_count--;
 
 	if(wale_p->has_internal_lock)
 		pthread_mutex_unlock(get_wale_lock(wale_p));
@@ -103,7 +103,7 @@ uint64_t get_next_log_sequence_number_of(wale* wale_p, uint64_t log_sequence_num
 
 	pthread_mutex_lock(get_wale_lock(wale_p));
 
-	wale_p->random_readers_count++;
+	wale_p->random_readers_count--;
 
 	if(wale_p->has_internal_lock)
 		pthread_mutex_unlock(get_wale_lock(wale_p));
@@ -144,7 +144,7 @@ uint64_t get_prev_log_sequence_number_of(wale* wale_p, uint64_t log_sequence_num
 
 	pthread_mutex_lock(get_wale_lock(wale_p));
 
-	wale_p->random_readers_count++;
+	wale_p->random_readers_count--;
 
 	if(wale_p->has_internal_lock)
 		pthread_mutex_unlock(get_wale_lock(wale_p));
@@ -194,7 +194,7 @@ void* get_log_record_at(wale* wale_p, uint64_t log_sequence_number, uint32_t* lo
 
 	pthread_mutex_lock(get_wale_lock(wale_p));
 
-	wale_p->random_readers_count++;
+	wale_p->random_readers_count--;
 
 	if(wale_p->has_internal_lock)
 		pthread_mutex_unlock(get_wale_lock(wale_p));
@@ -202,7 +202,28 @@ void* get_log_record_at(wale* wale_p, uint64_t log_sequence_number, uint32_t* lo
 	return log_record;
 }
 
-uint64_t append_log_record(wale* wale_p, const void* log_record, uint32_t log_record_size, int is_check_point);
+uint64_t append_log_record(wale* wale_p, const void* log_record, uint32_t log_record_size, int is_check_point)
+{
+	if(wale_p->has_internal_lock)
+		pthread_mutex_lock(get_wale_lock(wale_p));
+
+	// take slot if the next log sequence number is in the append only buffer
+
+	wale_p->append_only_writers_count++;
+
+	pthread_mutex_unlock(get_wale_lock(wale_p));
+
+	// TODO
+
+	pthread_mutex_lock(get_wale_lock(wale_p));
+
+	wale_p->append_only_writers_count--;
+
+	if(wale_p->has_internal_lock)
+		pthread_mutex_unlock(get_wale_lock(wale_p));
+
+	return log_record;
+}
 
 uint64_t flush_all_log_records(wale* wale_p);
 
