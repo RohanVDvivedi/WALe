35f494bbd1be8fd489191a7841980b70f865005a
RohanVDvivedi <rohandvivedi@gmail.com>
2023-07-03 00:07:26 -0700

    minor addition of comemnt

--- inc/storage_byte_ordering.h
+++ inc/storage_byte_ordering.h
@@ -1,10 +1,12 @@
 #ifndef STORAGE_BYTE_ORDERING_H
 #define STORAGE_BYTE_ORDERING_H
 
-void serialize_uint64(char bytes* bytes, uint64_t val);
-void serialize_uint32(char bytes* bytes, uint32_t val);
+// serialize and deserialize unaligned bytes from their little endian to host byte ordering
 
-uint64_t deserialize_uint64(const char* bytes);
-uint32_t deserialize_uint32(const char* bytes);
+void serialize_le_uint64(char bytes* bytes, uint64_t val);
+void serialize_le_uint32(char bytes* bytes, uint32_t val);
+
+uint64_t deserialize_le_uint64(const char* bytes);
+uint32_t deserialize_le_uint32(const char* bytes);
 
 #endif
--- src/storage_byte_ordering.c
+++ src/storage_byte_ordering.c
@@ -1,1 +1,7 @@
 #include<storage_byte_ordering.h>
+
+void serialize_uint64(char bytes* bytes, uint64_t val);
+void serialize_uint32(char bytes* bytes, uint32_t val);
+
+uint64_t deserialize_uint64(const char* bytes);
+uint32_t deserialize_uint32(const char* bytes);
