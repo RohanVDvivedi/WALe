02dfbc61c92c869e50aa4c1e85fc87d5953372a9
RohanVDvivedi <rohandvivedi@gmail.com>
2023-07-03 07:48:06 -0700

    implemeneted get log record at function

--- src/wale.c
+++ src/wale.c
@@ -1,12 +1,11 @@
 #include<wale.h>
 
 #include<wale_get_lock_util.h>
-
 #include<storage_byte_ordering.h>
-
 #include<random_read_util.h>
-
 #include<append_only_write_util.h>
+
+#include<stdlib.h>
 
 uint64_t get_first_log_sequence_number(wale* wale_p)
 {
@@ -124,7 +123,7 @@ uint64_t get_prev_log_sequence_number_of(wale* wale_p, uint64_t log_sequence_num
 	// set it to INVALID_LOG_SEQUENCE_NUMBER, which is default result
 	uint64_t prev_log_sequence_number = INVALID_LOG_SEQUENCE_NUMBER;
 
-	// if the wale has any records, and its first < log_sequence_number <= last, then read the size and add it to the log_sequence_number to get its next
+	// if the wale has any records, and its first < log_sequence_number <= last, then read the size of the previous log record and substract it from the log_sequence_number to get its prev
 	if(wale_p->on_disk_master_record.first_log_sequence_number != INVALID_LOG_SEQUENCE_NUMBER &&
 		wale_p->on_disk_master_record.first_log_sequence_number < log_sequence_number && 
 		log_sequence_number <= wale_p->on_disk_master_record.last_flushed_log_sequence_number
@@ -153,7 +152,55 @@ uint64_t get_prev_log_sequence_number_of(wale* wale_p, uint64_t log_sequence_num
 	return prev_log_sequence_number;
 }
 
-void* get_log_record_at(wale* wale_p, uint64_t log_sequence_number, uint32_t* log_record_size);
+void* get_log_record_at(wale* wale_p, uint64_t log_sequence_number, uint32_t* log_record_size)
+{
+	if(wale_p->has_internal_lock)
+		pthread_mutex_lock(get_wale_lock(wale_p));
+
+	wale_p->random_readers_count++;
+
+	pthread_mutex_unlock(get_wale_lock(wale_p));
+
+	// set it to NULL, which is default result
+	void* log_record = NULL;
+
+	// if the wale has any records, and its first <= log_sequence_number <= last
+	if(wale_p->on_disk_master_record.first_log_sequence_number != INVALID_LOG_SEQUENCE_NUMBER &&
+		wale_p->on_disk_master_record.first_log_sequence_number <= log_sequence_number && 
+		log_sequence_number <= wale_p->on_disk_master_record.last_flushed_log_sequence_number
+		)
+	{
+		uint64_t file_offset_of_log_record = log_sequence_number - wale_p->on_disk_master_record.first_log_sequence_number + wale_p->block_io_functions.block_size;
+
+		char size_of_log_record_bytes[4];
+		if(!random_read_at(size_of_log_record_bytes, 4, file_offset_of_log_record, &(wale_p->block_io_functions)))
+			goto FAILED;
+
+		(*log_record_size) = deserialize_le_uint32(size_of_log_record_bytes);
+
+		void* log_record = malloc((*log_record_size));
+		if(log_record == NULL)
+			goto FAILED;
+
+		if(!random_read_at(log_record, (*log_record_size), file_offset_of_log_record + 4, &(wale_p->block_io_functions)))
+		{
+			free(log_record);
+			log_record = NULL;
+			goto FAILED;
+		}
+
+		FAILED:;
+	}
+
+	pthread_mutex_lock(get_wale_lock(wale_p));
+
+	wale_p->random_readers_count++;
+
+	if(wale_p->has_internal_lock)
+		pthread_mutex_unlock(get_wale_lock(wale_p));
+
+	return log_record;
+}
 
 uint64_t append_log_record(wale* wale_p, const void* log_record, uint32_t log_record_size, int is_check_point);
 
