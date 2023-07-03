be613bb189d05381d605905086af4e684f3545be
RohanVDvivedi <rohandvivedi@gmail.com>
2023-07-03 00:17:12 -0700

    added serialization and deserialization logic for wale

--- inc/storage_byte_ordering.h
+++ inc/storage_byte_ordering.h
@@ -1,10 +1,12 @@
 #ifndef STORAGE_BYTE_ORDERING_H
 #define STORAGE_BYTE_ORDERING_H
 
+#include<inttypes.h>
+
 // serialize and deserialize unaligned bytes from their little endian to host byte ordering
 
-void serialize_le_uint64(char bytes* bytes, uint64_t val);
-void serialize_le_uint32(char bytes* bytes, uint32_t val);
+void serialize_le_uint64(char* bytes, uint64_t val);
+void serialize_le_uint32(char* bytes, uint32_t val);
 
 uint64_t deserialize_le_uint64(const char* bytes);
 uint32_t deserialize_le_uint32(const char* bytes);
--- src/storage_byte_ordering.c
+++ src/storage_byte_ordering.c
@@ -1,7 +1,35 @@
 #include<storage_byte_ordering.h>
 
-void serialize_uint64(char bytes* bytes, uint64_t val);
-void serialize_uint32(char bytes* bytes, uint32_t val);
+void serialize_le_uint64(char* bytes, uint64_t val)
+{
+	for(int i = 0; i < 8; i++, val >>= 8)
+		bytes[i] = val & UINT64_C(0xff);
+}
 
-uint64_t deserialize_uint64(const char* bytes);
-uint32_t deserialize_uint32(const char* bytes);
+void serialize_le_uint32(char* bytes, uint32_t val)
+{
+	for(int i = 0; i < 4; i++, val >>= 8)
+		bytes[i] = val & UINT32_C(0xff);
+}
+
+uint64_t deserialize_le_uint64(const char* bytes)
+{
+	uint64_t val = 0;
+	for(int i = 8; i > 0;)
+	{
+		--i;
+		val = val | ((bytes[i] & UINT64_C(0xff)) << (i * 8));
+	}
+	return val;
+}
+
+uint32_t deserialize_le_uint32(const char* bytes)
+{
+	uint64_t val = 0;
+	for(int i = 4; i > 0;)
+	{
+		--i;
+		val = val | ((bytes[i] & UINT32_C(0xff)) << (i * 8));
+	}
+	return val;
+}
