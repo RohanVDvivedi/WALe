190373c2b770d0a7b0554da7164eac5909d5b181
RohanVDvivedi <rohandvivedi@gmail.com>
2023-07-03 07:16:40 -0700

    implemented basic master record read api of wale

--- src/wale.c
+++ src/wale.c
@@ -7,19 +7,63 @@ uint64_t get_first_log_sequence_number(wale* wale_p)
 	if(wale_p->has_internal_lock)
 		pthread_mutex_lock(get_wale_lock(wale_p));
 
+	wale_p->random_readers_count++;
+
 	pthread_mutex_unlock(get_wale_lock(wale_p));
 
-	// TODO
+	uint64_t first_log_sequence_number = wale_p->on_disk_master_record.first_log_sequence_number;
 
 	pthread_mutex_lock(get_wale_lock(wale_p));
 
+	wale_p->random_readers_count++;
+
 	if(wale_p->has_internal_lock)
 		pthread_mutex_unlock(get_wale_lock(wale_p));
+
+	return first_log_sequence_number;
 }
 
-uint64_t get_last_flushed_log_sequence_number(wale* wale_p);
+uint64_t get_last_flushed_log_sequence_number(wale* wale_p)
+{
+	if(wale_p->has_internal_lock)
+		pthread_mutex_lock(get_wale_lock(wale_p));
 
-uint64_t get_check_point_log_sequence_number(wale* wale_p);
+	wale_p->random_readers_count++;
+
+	pthread_mutex_unlock(get_wale_lock(wale_p));
+
+	uint64_t last_flushed_log_sequence_number = wale_p->on_disk_master_record.last_flushed_log_sequence_number;
+
+	pthread_mutex_lock(get_wale_lock(wale_p));
+
+	wale_p->random_readers_count++;
+
+	if(wale_p->has_internal_lock)
+		pthread_mutex_unlock(get_wale_lock(wale_p));
+
+	return last_flushed_log_sequence_number;
+}
+
+uint64_t get_check_point_log_sequence_number(wale* wale_p)
+{
+	if(wale_p->has_internal_lock)
+		pthread_mutex_lock(get_wale_lock(wale_p));
+
+	wale_p->random_readers_count++;
+
+	pthread_mutex_unlock(get_wale_lock(wale_p));
+
+	uint64_t check_point_log_sequence_number = wale_p->on_disk_master_record.check_point_log_sequence_number;
+
+	pthread_mutex_lock(get_wale_lock(wale_p));
+
+	wale_p->random_readers_count++;
+
+	if(wale_p->has_internal_lock)
+		pthread_mutex_unlock(get_wale_lock(wale_p));
+
+	return check_point_log_sequence_number;
+}
 
 uint64_t get_next_log_sequence_number_of(wale* wale_p, uint64_t log_sequence_number);
 
