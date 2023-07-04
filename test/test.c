dd678a0e9d57654fe2366eaa6d526dd8359ddec3
RohanVDvivedi <rohandvivedi@gmail.com>
2023-07-04 02:11:46 -0700

    added utily functions to call log test cases

--- test/test.c
+++ test/test.c
@@ -18,12 +18,33 @@
 
 block_io_ops get_block_io_functions(const block_file* bf);
 
-char log_buffer[4096];
-
 #define APPENDS_TO_PERFORM 512
 
-uint64_t log_sequences_numbers_count = 0;
-uint64_t log_sequences_numbers[APPENDS_TO_PERFORM];
+wale walE;
+
+void append_test_log()
+{
+	char log_buffer[4096];
+	sprintf(log_buffer, LOG_FORMAT, (((unsigned int)rand()) % 52), NUMBERS, rand());
+	uint64_t log_sequence_number = append_log_record(&walE, log_buffer, strlen(log_buffer) + 1, 0);
+	printf("log sequence number written = %" PRIu64 " : %s\n", log_sequence_number, log_buffer);
+}
+
+void print_all_flushed_logs()
+{
+	uint64_t log_sequence_number = get_first_log_sequence_number(&walE);
+	printf("first_log_sequence_numbers = %"PRIu64"\n", log_sequence_number);
+	printf("check_point_log_sequence_numbers = %"PRIu64"\n", get_check_point_log_sequence_number(&walE));
+	while(log_sequence_number != INVALID_LOG_SEQUENCE_NUMBER)
+	{
+		uint32_t log_record_size;
+		char* log_record = (char*) get_log_record_at(&walE, log_sequence_number, &log_record_size);
+		printf("%" PRIu64 " : <%s>\n", log_sequence_number, log_record);
+		free(log_record);
+		log_sequence_number = get_next_log_sequence_number_of(&walE, log_sequence_number);
+	}
+	printf("last_flushed_log_sequence_numbers = %"PRIu64"\n\n", get_last_flushed_log_sequence_number(&walE));
+}
 
 int main()
 {
@@ -36,7 +57,6 @@ int main()
 		return -1;
 	}
 
-	wale walE;
 	if(!initialize_wale(&walE, (new_file ? 7 : INVALID_LOG_SEQUENCE_NUMBER), NULL, get_block_io_functions(&bf), APPEND_ONLY_BUFFER_COUNT))
 	{
 		printf("failed to create wale instance\n");
@@ -44,15 +64,12 @@ int main()
 		return -1;
 	}
 
-	sprintf(log_buffer, LOG_FORMAT, (((unsigned int)rand()) % 52), NUMBERS, rand());
-	log_sequences_numbers[log_sequences_numbers_count++] = append_log_record(&walE, log_buffer, strlen(log_buffer) + 1, 0);
-	printf("log sequence number written = %" PRIu64 " : %s\n", log_sequences_numbers[log_sequences_numbers_count-1], log_buffer);
-
-	sprintf(log_buffer, LOG_FORMAT, (((unsigned int)rand()) % 52), NUMBERS, rand());
-	log_sequences_numbers[log_sequences_numbers_count++] = append_log_record(&walE, log_buffer, strlen(log_buffer) + 1, 0);
-	printf("log sequence number written = %" PRIu64 " : %s\n", log_sequences_numbers[log_sequences_numbers_count-1], log_buffer);
+	append_test_log();
+	append_test_log();
 
 	printf("flushed until = %" PRIu64 "\n", flush_all_log_records(&walE));
+
+	print_all_flushed_logs();
 
 	deinitialize_wale(&walE);
 
