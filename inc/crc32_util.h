2cec4c0d21fb4efdcac1109f7c5244cd0cbbff37
RohanVDvivedi <rohandvivedi@gmail.com>
2023-07-08 03:18:46 -0700

    added declaration for crc32 function calculation

---
+++ inc/crc32_util.h
@@ -0,0 +1,6 @@
+#ifndef CRC32_UTIL_H
+#define CRC32_UTIL_H
+
+uint32_t crc32(uint32_t crc32, const void* data, uint64_t data_size);
+
+#endif
---
+++ src/crc32_util.c
@@ -0,0 +1,6 @@
+#include<crc32_util.h>
+
+uint32_t crc32(uint32_t crc32, const void* data, uint64_t data_size)
+{
+	// TODO
+}
