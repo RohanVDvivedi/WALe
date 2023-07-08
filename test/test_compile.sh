e1ec80bdf6761cd183ea4eb1ff895c20c165623a
RohanVDvivedi <rohandvivedi@gmail.com>
2023-07-08 09:31:10 -0700

    added commnd to read the log file with decimal addresses in binary

--- test/test_compile.sh
+++ test/test_compile.sh
@@ -8,3 +8,6 @@ gcc ./test_prwrite_validate.c ./test_util.c -o prwrite_validate.out -I./ -lblock
 
 # use below command to change a byte anywhere in the file and see, how crc32 identifies this error
 # printf '\x31' | dd of=test_blob bs=1 seek=100 count=1 conv=notrunc
+
+# to pretty print the .log file you can use
+#hexdump -e'"%07.8_ad  " 8/1 "%02x " "  " 8/1 "%02x " "  |"' -e'16/1  "%_p"  "|\n"' test.log
