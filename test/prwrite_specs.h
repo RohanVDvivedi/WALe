6d2f1416cac9d3714e3dbd14cff007f901167e9c
RohanVDvivedi <rohandvivedi@gmail.com>
2023-07-09 05:32:23 -0700

    testing WALe with large thread count and large log count

--- test/prwrite_specs.h
+++ test/prwrite_specs.h
@@ -3,8 +3,8 @@
 
 // specs for prwrite test cases
 
-#define THREAD_COUNT 6
-#define LOGS_PER_THREAD 64
-#define FLUSH_EVERY_LOGS_PER_THREAD 11
+#define THREAD_COUNT 256
+#define LOGS_PER_THREAD 256
+#define FLUSH_EVERY_LOGS_PER_THREAD 97
 
 #endif
