33131e9bb0abf4c62e60b9f8cd054c1e1fa90fe4
RohanVDvivedi <rohandvivedi@gmail.com>
2023-07-09 01:46:06 -0700

    --

--- test/test_compile.sh
+++ test/test_compile.sh
@@ -10,4 +10,4 @@ gcc ./test_prwrite_validate.c ./test_util.c -o prwrite_validate.out -I./ -lblock
 # printf '\x31' | dd of=test_blob bs=1 seek=100 count=1 conv=notrunc
 
 # to pretty print the .log file you can use
-#hexdump -e'"%07.8_ad  " 8/1 "%02x " "  " 8/1 "%02x " "  |"' -e'16/1  "%_p"  "|\n"' test.log
+# hexdump -e'"%07.8_ad  " 8/1 "%02x " "  " 8/1 "%02x " "  |"' -e'16/1  "%_p"  "|\n"' test.log
