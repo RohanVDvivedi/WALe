2b34c51b8c94a39bfab6c125b700c4a0e3823941
RohanVDvivedi <rohandvivedi@gmail.com>
2023-10-03 15:39:15 -0700

    minor change in comment

--- inc/util_master_record_io.h
+++ inc/util_master_record_io.h
@@ -5,7 +5,7 @@
 
 // no locks are acquired or released by the below functions
 // as expected, since it does not even have reference to any of the wale locks
-// it operated only with the master record provided, reading, writing and flushing it to the underlying disk using the block_io_functions
+// it operates only with the master record provided, reading, writing and flushing it to the underlying disk using the block_io_functions
 
 int read_master_record(master_record* mr, const block_io_ops* block_io_functions);
 
