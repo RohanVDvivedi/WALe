f6aa36c8fdbd3054152eabfd4e60a3c7073dde61
RohanVDvivedi <rohandvivedi@gmail.com>
2023-10-03 16:27:53 -0700

    master record io refactored to use log_seq_nr

--- src/util_master_record_io.c
+++ src/util_master_record_io.c
@@ -18,10 +18,11 @@ int read_master_record(master_record* mr, const block_io_ops* block_io_functions
 	}
 
 	// deserialize
-	mr->first_log_sequence_number = deserialize_le_uint64(mr_serial, sizeof(uint64_t));
-	mr->last_flushed_log_sequence_number = deserialize_le_uint64(mr_serial + sizeof(uint64_t), sizeof(uint64_t));
-	mr->check_point_log_sequence_number = deserialize_le_uint64(mr_serial + 2 * sizeof(uint64_t), sizeof(uint64_t));
-	mr->next_log_sequence_number = deserialize_le_uint64(mr_serial + 3 * sizeof(uint64_t), sizeof(uint64_t));
+	mr->log_sequence_number_width = deserialize_le_uint32(mr_serial, sizeof(uint32_t));
+	mr->first_log_sequence_number = deserialize_log_seq_nr(mr_serial + sizeof(uint32_t), mr->log_sequence_number_width);
+	mr->last_flushed_log_sequence_number = deserialize_log_seq_nr(mr_serial + sizeof(uint32_t) + mr->log_sequence_number_width, mr->log_sequence_number_width);
+	mr->check_point_log_sequence_number = deserialize_log_seq_nr(mr_serial + sizeof(uint32_t) + 2 * mr->log_sequence_number_width, mr->log_sequence_number_width);
+	mr->next_log_sequence_number = deserialize_log_seq_nr(mr_serial + sizeof(uint32_t) + 3 * mr->log_sequence_number_width, mr->log_sequence_number_width);
 
 	free(mr_serial);
 	return 1;
@@ -34,10 +35,11 @@ int write_and_flush_master_record(const master_record* mr, const block_io_ops* b
 		return 0;
 
 	// serialize
-	serialize_le_uint64(mr_serial, sizeof(uint64_t), mr->first_log_sequence_number);
-	serialize_le_uint64(mr_serial + sizeof(uint64_t), sizeof(uint64_t), mr->last_flushed_log_sequence_number);
-	serialize_le_uint64(mr_serial + 2 * sizeof(uint64_t), sizeof(uint64_t), mr->check_point_log_sequence_number);
-	serialize_le_uint64(mr_serial + 3 * sizeof(uint64_t), sizeof(uint64_t), mr->next_log_sequence_number);
+	serialize_le_uint32(mr_serial, sizeof(uint32_t), mr->log_sequence_number_width);
+	serialize_log_seq_nr(mr_serial + sizeof(uint32_t), mr->log_sequence_number_width, mr->first_log_sequence_number);
+	serialize_log_seq_nr(mr_serial + sizeof(uint32_t) + mr->log_sequence_number_width, mr->log_sequence_number_width, mr->last_flushed_log_sequence_number);
+	serialize_log_seq_nr(mr_serial + sizeof(uint32_t) + 2 * mr->log_sequence_number_width, mr->log_sequence_number_width, mr->check_point_log_sequence_number);
+	serialize_log_seq_nr(mr_serial + sizeof(uint32_t) + 3 * mr->log_sequence_number_width, mr->log_sequence_number_width, mr->next_log_sequence_number);
 
 	int io_success = block_io_functions->write_blocks(block_io_functions->block_io_ops_handle, mr_serial, 0, 1)
 						&& block_io_functions->flush_all_writes(block_io_functions->block_io_ops_handle);
