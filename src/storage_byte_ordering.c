d668b767c4ed37c3655e98e50e09509d84f8b5d9
RohanVDvivedi <rohandvivedi@gmail.com>
2023-10-03 09:54:07 -0700

    implemented serialize and deserialize functions with variable byte length serialization and deserialization

--- src/storage_byte_ordering.c
+++ src/storage_byte_ordering.c
@@ -1,35 +1,41 @@
 #include<storage_byte_ordering.h>
 
-void serialize_le_uint64(char* bytes, uint64_t val)
+#include<cutlery_math.h>
+
+void serialize_le_uint64(char* bytes, uint32_t bytes_size, uint64_t val)
 {
-	for(int i = 0; i < 8; i++, val >>= 8)
+	bytes_size = min(bytes_size, sizeof(uint64_t));
+	for(int i = 0; i < bytes_size; i++, val >>= CHAR_BIT)
 		bytes[i] = val & UINT64_C(0xff);
 }
 
-void serialize_le_uint32(char* bytes, uint32_t val)
+void serialize_le_uint32(char* bytes, uint32_t bytes_size, uint32_t val)
 {
-	for(int i = 0; i < 4; i++, val >>= 8)
+	bytes_size = min(bytes_size, sizeof(uint32_t));
+	for(int i = 0; i < bytes_size; i++, val >>= CHAR_BIT)
 		bytes[i] = val & UINT32_C(0xff);
 }
 
-uint64_t deserialize_le_uint64(const char* bytes)
+uint64_t deserialize_le_uint64(const char* bytes, uint32_t bytes_size)
 {
+	bytes_size = min(bytes_size, sizeof(uint64_t));
 	uint64_t val = 0;
-	for(int i = 8; i > 0;)
+	for(uint32_t i = bytes_size; i > 0;)
 	{
 		--i;
-		val = val | ((bytes[i] & UINT64_C(0xff)) << (i * 8));
+		val = val | ((bytes[i] & UINT64_C(0xff)) << (i * CHAR_BIT));
 	}
 	return val;
 }
 
-uint32_t deserialize_le_uint32(const char* bytes)
+uint32_t deserialize_le_uint32(const char* bytes, uint32_t bytes_size)
 {
-	uint64_t val = 0;
-	for(int i = 4; i > 0;)
+	bytes_size = min(bytes_size, sizeof(uint32_t));
+	uint32_t val = 0;
+	for(uint32_t i = bytes_size; i > 0;)
 	{
 		--i;
-		val = val | ((bytes[i] & UINT32_C(0xff)) << (i * 8));
+		val = val | ((bytes[i] & UINT32_C(0xff)) << (i * CHAR_BIT));
 	}
 	return val;
 }
