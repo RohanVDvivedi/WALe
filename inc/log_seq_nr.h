d01e5a33201d6c6ca29c0b1a537fe8530abd9f47
RohanVDvivedi <rohandvivedi@gmail.com>
2023-10-03 09:28:41 -0700

    added header for log_seq_nr

---
+++ inc/log_seq_nr.h
@@ -0,0 +1,38 @@
+#ifndef LOG_SEQ_NR_H
+#define LOG_SEQ_NR_H
+
+// this can be any value, but 2 is just right for all practical purposes
+// if you modify this, be sure to modify LOG_SEQ_NR_MAX
+#define LOG_SEQ_NR_LIMBS_COUNT 2
+
+#define LOG_SEQ_NR_MAX_BYTES   (LOG_SEQ_NR_LIMBS_COUNT * sizeof(uint64_t))
+
+typedef struct log_seq_ne log_seq_nr;
+struct log_seq_nr
+{
+	uint64_t limbs[LOG_SEQ_NR_LIMBS_COUNT];
+};
+
+#define get_log_seq_nr(val) ((log_seq_nr){.limbs = {val}})
+
+#define LOG_SEQ_NR_MIN ((log_seq_nr){.limbs = {}})
+#define LOG_SEQ_NR_MAX ((log_seq_nr){.limbs = {UINT64_MAX, UINT64_MAX}})
+
+int compare_log_seg_nr(log_seq_nr a, log_seq_nr b);
+
+// res will be set with addition (a + b), on success (i.e. return 1)
+// failure happens in case of an overflow OR if the result is strictly greater than max_limit
+int add_log_seq_nr(log_seq_nr* res, log_seq_nr a, log_seq_nr b, log_seq_nr max_limit);
+
+// res will be set with subtraction, on success (i.e. return 1)
+// failure happens in case of an underflow, when (a < b)
+int sub_log_seq_nr(log_seq_nr* res, log_seq_nr a, log_seq_nr b);
+
+// serialize and deserialize log_seq_nr-s
+void serialize_log_seq_nr(void* bytes, uint32_t bytes_size, log_seq_nr l);
+log_seq_nr deserialize_le_uint64(const char* bytes, uint32_t bytes_size);
+
+// print log_seq_nr
+void print_log_seq_nr(log_seq_nr l);
+
+#endif
