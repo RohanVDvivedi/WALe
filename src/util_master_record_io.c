229aec36bd7b66af3164ccf8a6a30abc3db123a3
RohanVDvivedi <rohandvivedi@gmail.com>
2023-10-04 15:57:15 -0700

    bugs resolved

--- src/util_master_record_io.c
+++ src/util_master_record_io.c
@@ -22,10 +22,6 @@ int read_master_record(master_record* mr, const block_io_ops* block_io_functions
 		return 0;
 	}
 
-	// calculate crc32 for master record
-	uint32_t calculated_crc32 = crc32_init();
-	calculated_crc32 = crc32_util(calculated_crc32, mr_serial, sizeof(uint32_t) + 4 * mr->log_sequence_number_width);
-
 	// deserialize
 	mr->log_sequence_number_width = deserialize_le_uint32(mr_serial, sizeof(uint32_t));
 	mr->first_log_sequence_number = deserialize_log_seq_nr(mr_serial + sizeof(uint32_t), mr->log_sequence_number_width);
@@ -34,6 +30,10 @@ int read_master_record(master_record* mr, const block_io_ops* block_io_functions
 	mr->next_log_sequence_number = deserialize_log_seq_nr(mr_serial + sizeof(uint32_t) + 3 * mr->log_sequence_number_width, mr->log_sequence_number_width);
 
 	uint32_t parsed_crc32 = deserialize_le_uint32(mr_serial + sizeof(uint32_t) + 4 * mr->log_sequence_number_width, sizeof(uint32_t));
+
+	// calculate crc32 for master record, NOTE :: we can not calculate crc32 without reading the log_sequence_number_width
+	uint32_t calculated_crc32 = crc32_init();
+	calculated_crc32 = crc32_util(calculated_crc32, mr_serial, sizeof(uint32_t) + 4 * mr->log_sequence_number_width);
 
 	free(mr_serial);
 
