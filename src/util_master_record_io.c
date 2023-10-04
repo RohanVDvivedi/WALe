fe1d4f452a3279374994bbfac33469b7ccdc839b
RohanVDvivedi <rohandvivedi@gmail.com>
2023-10-04 12:28:04 -0700

    calculating and writing crc32 for master record

--- src/util_master_record_io.c
+++ src/util_master_record_io.c
@@ -1,8 +1,9 @@
 #include<util_master_record_io.h>
 
-#include<stdlib.h>
-
 #include<storage_byte_ordering.h>
+#include<crc32_util.h>
+
+#include<stdlib.h>
 
 int read_master_record(master_record* mr, const block_io_ops* block_io_functions, int* error)
 {
@@ -41,12 +42,19 @@ int write_and_flush_master_record(const master_record* mr, const block_io_ops* b
 		return 0;
 	}
 
+	// calculate crc32 for master record
+	uint32_t calculated_crc32 = crc32_init();
+	calculated_crc32 = crc32_util(calculated_crc32, mr_serial, sizeof(uint32_t) + 4 * mr->log_sequence_number_width);
+
 	// serialize
 	serialize_le_uint32(mr_serial, sizeof(uint32_t), mr->log_sequence_number_width);
 	serialize_log_seq_nr(mr_serial + sizeof(uint32_t), mr->log_sequence_number_width, mr->first_log_sequence_number);
 	serialize_log_seq_nr(mr_serial + sizeof(uint32_t) + mr->log_sequence_number_width, mr->log_sequence_number_width, mr->last_flushed_log_sequence_number);
 	serialize_log_seq_nr(mr_serial + sizeof(uint32_t) + 2 * mr->log_sequence_number_width, mr->log_sequence_number_width, mr->check_point_log_sequence_number);
 	serialize_log_seq_nr(mr_serial + sizeof(uint32_t) + 3 * mr->log_sequence_number_width, mr->log_sequence_number_width, mr->next_log_sequence_number);
+
+	// write calculated_crc32 on the mr_serial
+	serialize_le_uint32(mr_serial + sizeof(uint32_t) + 4 * mr->log_sequence_number_width, sizeof(uint32_t), calculated_crc32);
 
 	int io_success = block_io_functions->write_blocks(block_io_functions->block_io_ops_handle, mr_serial, 0, 1)
 						&& block_io_functions->flush_all_writes(block_io_functions->block_io_ops_handle);
