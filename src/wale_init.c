b51e11abbe196d0b4e56594930e6285bc4402c62
RohanVDvivedi <rohandvivedi@gmail.com>
2023-10-12 18:30:55 -0700

    allowing to open wale with 0 number of append only buffers

--- src/wale_init.c
+++ src/wale_init.c
@@ -42,59 +42,67 @@ int initialize_wale(wale* wale_p, uint32_t log_sequence_number_width, log_seq_nr
 			return 0;
 	}
 
-	wale_p->in_memory_master_record = wale_p->on_disk_master_record;
-
-	wale_p->buffer = aligned_alloc(wale_p->block_io_functions.block_buffer_alignment, (append_only_block_count * wale_p->block_io_functions.block_size));
-	wale_p->buffer_block_count = append_only_block_count;
-
-	if(wale_p->buffer == NULL)
-	{
-		(*error) = ALLOCATION_FAILED;
-		return 0;
-	}
-
-	// there are no log records on the disk, if the first_log_sequence_number == INVALID_LOG_SEQUENCE_NUMBER
-	if(are_equal_log_seq_nr(wale_p->in_memory_master_record.first_log_sequence_number, INVALID_LOG_SEQUENCE_NUMBER))
-	{
-		wale_p->append_offset = 0;
-		wale_p->buffer_start_block_id = 1;
-	}
-	else // else read appropriate first block from memory
-	{
-		// calculate file_offset to start appending from
-		uint64_t file_offset_to_append_from;
-		{
-			log_seq_nr temp; // = next_log_sequence_number - first_log_sequence_number + block_size
-			if(	(!sub_log_seq_nr(&temp, wale_p->in_memory_master_record.next_log_sequence_number, wale_p->in_memory_master_record.first_log_sequence_number)) ||
-				(!add_log_seq_nr(&temp, temp, get_log_seq_nr(wale_p->block_io_functions.block_size), LOG_SEQ_NR_MIN)) ||
-				(!cast_to_uint64(&file_offset_to_append_from, temp)) )
-			{
-				// this implies master record is corrupted
-				free(wale_p->buffer);
-				return 0;
-			}
-		}
-
-		wale_p->append_offset = file_offset_to_append_from % wale_p->block_io_functions.block_size;
-		wale_p->buffer_start_block_id = UINT_ALIGN_DOWN(file_offset_to_append_from, wale_p->block_io_functions.block_size) / wale_p->block_io_functions.block_size;
-
-		if(wale_p->append_offset)
-		{
-			if(!wale_p->block_io_functions.read_blocks(wale_p->block_io_functions.block_io_ops_handle, wale_p->buffer, wale_p->buffer_start_block_id, 1))
-			{
-				(*error) = READ_IO_ERROR;
-				free(wale_p->buffer);
-				return 0;
-			}
-		}
-	}
-
 	pthread_cond_init(&(wale_p->wait_for_scroll), NULL);
 	initialize_rwlock(&(wale_p->flushed_log_records_lock), get_wale_lock(wale_p));
 	initialize_rwlock(&(wale_p->append_only_buffer_lock), get_wale_lock(wale_p));
 
 	wale_p->max_limit = LOG_SEQ_NR_MIN;
 	set_bit_in_log_seq_nr(&(wale_p->max_limit), wale_p->in_memory_master_record.log_sequence_number_width * CHAR_BIT);
+
+	wale_p->in_memory_master_record = wale_p->on_disk_master_record;
+
+	if(append_only_block_count == 0) // WALe is opened only for reading
+	{
+		wale_p->buffer = NULL;
+		wale_p->buffer_block_count = 0;
+	}
+	else
+	{
+		wale_p->buffer = aligned_alloc(wale_p->block_io_functions.block_buffer_alignment, (append_only_block_count * wale_p->block_io_functions.block_size));
+		wale_p->buffer_block_count = append_only_block_count;
+
+		if(wale_p->buffer == NULL)
+		{
+			(*error) = ALLOCATION_FAILED;
+			return 0;
+		}
+
+		// there are no log records on the disk, if the first_log_sequence_number == INVALID_LOG_SEQUENCE_NUMBER
+		if(are_equal_log_seq_nr(wale_p->in_memory_master_record.first_log_sequence_number, INVALID_LOG_SEQUENCE_NUMBER))
+		{
+			wale_p->append_offset = 0;
+			wale_p->buffer_start_block_id = 1;
+		}
+		else // else read appropriate first block from memory
+		{
+			// calculate file_offset to start appending from
+			uint64_t file_offset_to_append_from;
+			{
+				log_seq_nr temp; // = next_log_sequence_number - first_log_sequence_number + block_size
+				if(	(!sub_log_seq_nr(&temp, wale_p->in_memory_master_record.next_log_sequence_number, wale_p->in_memory_master_record.first_log_sequence_number)) ||
+					(!add_log_seq_nr(&temp, temp, get_log_seq_nr(wale_p->block_io_functions.block_size), LOG_SEQ_NR_MIN)) ||
+					(!cast_to_uint64(&file_offset_to_append_from, temp)) )
+				{
+					// this implies master record is corrupted
+					free(wale_p->buffer);
+					return 0;
+				}
+			}
+
+			wale_p->append_offset = file_offset_to_append_from % wale_p->block_io_functions.block_size;
+			wale_p->buffer_start_block_id = UINT_ALIGN_DOWN(file_offset_to_append_from, wale_p->block_io_functions.block_size) / wale_p->block_io_functions.block_size;
+
+			if(wale_p->append_offset)
+			{
+				if(!wale_p->block_io_functions.read_blocks(wale_p->block_io_functions.block_io_ops_handle, wale_p->buffer, wale_p->buffer_start_block_id, 1))
+				{
+					(*error) = READ_IO_ERROR;
+					free(wale_p->buffer);
+					return 0;
+				}
+			}
+		}
+	}
 
 	return 1;
 }
