0812d522274e9cd0c0e3c963a697eec93a61cfb0
RohanVDvivedi <rohandvivedi@gmail.com>
2023-10-29 19:15:20 -0700

    test code will need serint

--- test/test_compile.sh
+++ test/test_compile.sh
@@ -1,10 +1,10 @@
-gcc ./test_swrite.c ./test_util.c -o swrite.out -lblockio -lwale -lrwlock -lpthread -lcutlery -lz
+gcc ./test_swrite.c ./test_util.c -o swrite.out -lblockio -lwale -lserint -lrwlock -lpthread -lcutlery -lz
 
-gcc ./test_read.c ./test_util.c -o read.out -lblockio -lwale -lrwlock -lpthread -lcutlery -lz
+gcc ./test_read.c ./test_util.c -o read.out -lblockio -lwale -lserint -lrwlock -lpthread -lcutlery -lz
 
-gcc ./test_prwrite.c ./test_util.c -o prwrite.out -I./ -lboompar -lblockio -lwale -lrwlock -lpthread -lcutlery -lz
+gcc ./test_prwrite.c ./test_util.c -o prwrite.out -I./ -lboompar -lblockio -lwale -lserint -lrwlock -lpthread -lcutlery -lz
 
-gcc ./test_prwrite_validate.c ./test_util.c -o prwrite_validate.out -I./ -lblockio -lwale -lrwlock -lpthread -lcutlery -lz
+gcc ./test_prwrite_validate.c ./test_util.c -o prwrite_validate.out -I./ -lblockio -lwale -lserint -lrwlock -lpthread -lcutlery -lz
 
 # use below command to change a byte anywhere in the file and see, how crc32 identifies this error
 # printf '\x31' | dd of=test_blob bs=1 seek=100 count=1 conv=notrunc
