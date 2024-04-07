7823d48044f5aa8d188322a6a521b3a2e3cac2e7
RohanVDvivedi <rohandvivedi@gmail.com>
2024-04-07 21:23:59 +0530

    --

--- src/block_io_ops_util.c
+++ src/block_io_ops_util.c
@@ -1,5 +1,7 @@
 #include<block_io_ops_util.h>
 
+#include<cutlery_stds.h>
+
 uint64_t get_block_id_from_file_offset(uint64_t file_offset, const block_io_ops* block_io_functions)
 {
 	return UINT_ALIGN_DOWN(file_offset, block_io_functions->block_size) / block_io_functions->block_size;
